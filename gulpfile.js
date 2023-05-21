84c140041d2f529c5b699d76b4a557d5ead223be
stasfilippov <kron888999@gmail.com>
2023-05-21 18:11:06 +0300

    add: tasks: convert fonts, images, sprites, html

--- gulpfile.js
+++ gulpfile.js
@@ -7,7 +7,63 @@ const uglify = require('gulp-uglify-es').default;
 const browserSync = require('browser-sync').create();
 const autoprefixer = require('gulp-autoprefixer');
 const clean = require('gulp-clean');
+const avif = require('gulp-avif');
+const webp = require('gulp-webp');
+const imagemin = require('gulp-imagemin');
+const newer = require('gulp-newer');
+const fonter = require('gulp-fonter');
+const ttf2woff2 = require('gulp-ttf2woff2');
+const svgSprite = require('gulp-svg-sprite');
+const include = require('gulp-include');
 
+function pages () {
+    return src('app/pages/*.html')
+        .pipe(include({
+            includePaths: 'app/components'
+        }))
+        .pipe(dest('app'))
+        .pipe(browserSync.stream())
+}
+
+function fonts () {
+    return src('app/fonts/src/*.*')
+        .pipe(fonter({
+            formats: ['woff', 'ttf']
+        }))
+        .pipe(src('app/fonts/*.ttf'))
+        .pipe(ttf2woff2())
+        .pipe(dest('app/fonts'))
+}
+
+
+function images() {
+    return src(['app/images/src/*.*', '!app/images/src/*.svg'])
+        .pipe(newer('app/images'))
+        .pipe(avif({ quality : 50 }))
+
+        .pipe(src('app/images/src/*.*'))
+        .pipe(newer('app/images'))
+        .pipe(webp())
+
+        .pipe(src('app/images/src/*.*'))
+        .pipe(newer('app/images'))
+        .pipe(imagemin())
+
+        .pipe(dest('app/images'))
+}
+
+function sprite () {
+    return src ('app/images/*.svg')
+        .pipe(svgSprite({
+            mode: {
+                stack: {
+                    sprite: '../sprite.svg',
+                    example: true
+                }
+            }
+        }))
+        .pipe(dest('app/images'))
+}
 
 //функция для преобразования файла .js в .min.js 
 function scripts () {
@@ -32,19 +88,18 @@ function styles () {
 
 //автоматический запуск функций styles и scripts
 function watching () {
-    watch (['app/scss/index.scss'], styles)
-    watch (['app/js/main.js'], scripts)
-    watch (['app/*.html']).on('change', browserSync.reload)
-}
-
-//автоматическое обновление страницы
-function browsersync() {
     browserSync.init({
         server: {
             baseDir: "app/"
         }
     });
+    watch (['app/scss/index.scss'], styles)
+    watch (['app/images/src'], images)
+    watch (['app/js/main.js'], scripts)
+    watch (['app/components/*', 'app/pages/*'], pages)
+    watch (['app/*.html']).on('change', browserSync.reload)
 }
+
 
 function cleanDist () {
     return src('dist')
@@ -54,6 +109,11 @@ function cleanDist () {
 function building() {
     return src([
         'app/css/style.min.css',
+        'app/images/*.*',
+        '!app/images/*.svg',
+        '!app/images/stack/sprite.stack.html',
+        'app/images/sprite.svg',
+        'app/fonts/*.*',
         'app/js/main.min.js',
         'app/**/*.html'
     ], {base : 'app'})
@@ -61,11 +121,14 @@ function building() {
 }
 
 
-
 exports.styles = styles;
+exports.images = images;
+exports.fonts = fonts;
+exports.pages = pages;
+exports.building = building;
+exports.sprite = sprite;
 exports.scripts = scripts;
 exports.watching = watching;
-exports.browsersync = browsersync;
 
 exports.build = series(cleanDist, building);
-exports.default = parallel(styles, scripts, browsersync, watching);
+exports.default = parallel(styles, images, scripts, pages, watching);
